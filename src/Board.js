b72b4000de9c067bd889c8bd6de76a477808c6cb
JSP <julianne.peters@gmail.com>
2018-12-16 18:44:10 -0800

    added function to create board and accept arguement of how many mines. will build out different board sizes. added counter for mines left based on flags dropped.:
    l;

--- src/Board.js
+++ src/Board.js
@@ -6,68 +6,132 @@ class Board extends Component {
     constructor(props){
         super(props)
         this.state = {
+            gameWon: true,
             board: Array(100).fill(-1),
             id: 1,
-            mines: [0, 1, 2, 10, 12, 20, 21, 22, 33, 44, 55, 66]
+            mines: [],
+            minesLeft: 0
         }
+    }
+
+    plantMines(num) {
+        let mineField = Array(num).fill(null)
+        mineField.map((el, i) => {
+            let tempMine = Math.floor(Math.random() * (99))
+            if (mineField.includes(tempMine)) {
+                return tempMine = Math.floor(Math.random() * (99))
+            } else {
+                return mineField[i] = tempMine
+            }
+        })
+        this.setState({
+            mines: mineField,
+            gameWon: false,
+            board: Array(100).fill(-1),
+            minesLeft: num
+        })
     }
 
     rightClick = (id) => {
-        let { board } = this.state
+        let { board, gameWon, minesLeft } = this.state
+        let mineCount = minesLeft
         let move = board
+        if (gameWon === false) {
             if (board[id] === -1) {
                 move[id] = 9
+                mineCount -= 1
+            } else if (board[id] === 9) {
+                move[id] = "?"
+                mineCount += 1
+            } else if (board[id] === "?") {
+                move[id] = -1
             }
-        this.setState({
-          board: move
-        })
-        console.log("right click");
+            this.setState({
+                board: move,
+                minesLeft: mineCount
+            })
+        }
     }
 
     playerTurn = (id) => {
-      let { board, mines } = this.state
-      let move = board
-        if (mines.includes(id)) {
-          move[id] = 10
-      } else if (board[id] === -1) {
-          let counter = 0
-          const num = id
-          let str = num.toString()
-          let lastDigit = str.charAt(str.length-1)
-          const surroundingSquaresIfNine = [id-11, id-10, id-1, id+9, id+10]
-          const surroundingSquares = [id-11, id-10, id-9, id-1, id+1, id+9, id+10, id+11]
-            if (lastDigit === "9") {
-              for (let i = 0; i < surroundingSquaresIfNine.length; i++) {
-                if (this.state.mines.includes(surroundingSquaresIfNine[i])) {
-                  counter ++
+        let { board, mines, gameWon } = this.state
+        let move = board
+        let gameOver = gameWon
+        // validating gamestate not won //
+        if (gameWon === false) {
+            // if click hits a mine //
+            if (mines.includes(id)) {
+                move[id] = 10
+                gameOver = true
+                this.gameLost(id)
+            } else if (board[id] === -1) {
+                let counter = 0
+                const num = id
+                let str = num.toString()
+                let lastDigit = str.charAt(str.length-1)
+                const surroundingSquaresIfNine = [id-11, id-10, id-1, id+9, id+10]
+                const surroundingSquares = [id-11, id-10, id-9, id-1, id+1, id+9, id+10, id+11]
+                // if
+                if (lastDigit === "9") {
+                    for (let i = 0; i < surroundingSquaresIfNine.length; i++) {
+                        if (this.state.mines.includes(surroundingSquaresIfNine[i])) {
+                      counter ++
+                        }
+                    }
+                } else if (lastDigit !== "9") {
+                    for (let i = 0; i < surroundingSquares.length; i++) {
+                        if (this.state.mines.includes(surroundingSquares[i])) {
+                            counter ++
+                        }
+                    }
                 }
-              }
-            } else if (lastDigit !== "9") {
-              for (let i = 0; i < surroundingSquares.length; i++) {
-                if (this.state.mines.includes(surroundingSquares[i])) {
-                  counter ++
-                }
-              }
+                move[id] = counter
             }
-          move[id] = counter
+            this.setState({
+              board: move,
+              gameWon: gameOver
+            })
         }
-        console.log(move);
+    }
+
+    gameLost(id) {
+        let { mines, board } = this.state
+        let tempBoard = board
+        mines.map(el => {
+            if(el !== id)
+            return tempBoard[el] = 11
+        })
         this.setState({
-          board: move
+            board:tempBoard
         })
     }
 
 
-  render() {
-    return (
-      <div id="board">
-        {this.state.board.map((el, i) => {
-            return <Square playerTurn={this.playerTurn} rightClick={this.rightClick} arrayVal={el} index={i}/>
-        })
-        }
-      </div>
-    );
-  }
+    render() {
+        console.log(this.state.mines);
+        return (
+            <main>
+                <section className='startGameContainer'>
+                    <div id='easyGame' className='startGame' onClick={() => this.plantMines(10)}>
+                        Easy
+                    </div>
+                    <div id='intermediateGame' className='startGame' onClick={() => this.plantMines(20)}>
+                        Intermediate
+                    </div>
+                    <div id='difficultGame' className='startGame' onClick={() => this.plantMines(35)}>
+                        Difficult
+                    </div>
+                </section>
+                <div>{this.state.minesLeft}</div>
+                <div id="board">
+                    {this.state.board.map((el, i) => {
+                        return <Square playerTurn={this.playerTurn} rightClick={this.rightClick} arrayVal={el} index={i}/>
+                    })
+                    }
+                </div>
+            </main>
+        );
+    }
 }
 
 export default Board;
--- src/Square.js
+++ src/Square.js
@@ -3,42 +3,52 @@ import React, { Component } from 'react';
 class Square extends Component {
 
     squareContent = (value) => {
-        if (value === -1 || value === 0 || value === 9 || value === 10) {
+        if (value === -1 || value === 0 || value === 9 || value === 10 || value === 11) {
             return false
         } else {
             return value
         }
     }
 
-  backgroundChooser = (value) => {
-      const BACKGROUNDS = ["url('../Images/square.png')", "url('../Images/flag.png')", "url('../Images/bomb.png')", ""]
-      if (value === -1) {
-          return BACKGROUNDS[0]
-      } else if (value === 2) {
-
-      }
-  }
-
-
-  render() {
-      const COLORS = [null, "blue", "green", "red", "purple", "maroon", "teal", "black", "gray"]
-    return (
-      <div
-        contextMenu="none"
-        id="square"
-        onContextMenu={
-            () => this.props.rightClick(this.props.index)}
-        onClick={
-            () => {this.props.playerTurn(this.props.index)}}
-        style={
-            {color:COLORS[this.props.arrayVal]},
-            {backgroundImage:this.backgroundChooser(this.props.arrayVal)}
+    backgroundChooser = (value) => {
+        const BACKGROUNDS = ["url('../Images/square.png')", "url('../Images/flagNew.png')", "url('../Images/mineNew.png')", "url('../Images/mineHit.png')", ""]
+        // miss //
+        if (value === -1) {
+            return BACKGROUNDS[0]
+        // miss but mines in adjacent squares //
+        } else if (value === 11) {
+            return BACKGROUNDS[2]
+        } else if (value === 1 || value === 2 || value === 3 || value === 4 || value === 5 || value === 6 || value === 7 || value === 8) {
+            return BACKGROUNDS[4]
+        // flag //
+        } else if (value === 9) {
+            return BACKGROUNDS[1]
+        // mine hit//
+        } else if (value === 10) {
+            return BACKGROUNDS[3]
+        // unfound mines //
         }
-      >
-        {this.squareContent(this.props.arrayVal)}
-      </div>
-    );
-  }
+    }
+
+
+    render() {
+        const COLORS = [null, "blue", "green", "red", "purple", "maroon", "teal", "black", "gray"]
+        return (
+            <div
+                contextMenu="none"
+                id="square"
+                onContextMenu={
+                    () => this.props.rightClick(this.props.index)}
+                onClick={
+                    () => {this.props.playerTurn(this.props.index)}}
+                style={
+                    {color:COLORS[this.props.arrayVal], backgroundImage:this.backgroundChooser(this.props.arrayVal)}
+                }
+            >
+                {this.squareContent(this.props.arrayVal)}
+            </div>
+        );
+    }
 }
 
 export default Square;
